7c21d6c70eef87b7d654e072540c343737cc1513
Aidan Neal <decator.c@proton.me>
2023-02-17 22:33:34 -0500

    Fix ascii art printing

--- src/printing/mod.rs
+++ src/printing/mod.rs
@@ -6,7 +6,9 @@ pub enum OsArt
 {
     ArchLinux,
     AlpineLinux,
+    Debian,
     Windows,
+    Unknown,
 }
 
 pub fn print_with_logo(os: OsArt, s: &str)
@@ -15,32 +17,11 @@ pub fn print_with_logo(os: OsArt, s: &str)
     {
         OsArt::ArchLinux => ARCH_LINUX,
         OsArt::AlpineLinux => ALPINE_LINUX,
-        _ => "",
+        OsArt::Debian => DEBIAN,
+        _ => UNKNOWN,
     };
 
-    let indentation = {
-        let mut indent = String::from("\t");
-        let (min, max) = min_max_line_len(art);
-        let space_diff = (max - min) / 8;
-
-        match space_diff.cmp(&0)
-        {
-            std::cmp::Ordering::Greater =>
-            {
-                for _ in 0..space_diff
-                {
-                    indent.push_str("  ");
-                }
-            }
-            std::cmp::Ordering::Equal => indent.push('\t'),
-            _ => (),
-        }
-
-        indent
-    };
-
-
-    println!("{}", with_both(art, s, &indentation));
+    print!("{}", with_both(art, s));
 }
 
 /// Returns the length of the longest line in the string
@@ -67,8 +48,10 @@ fn min_max_line_len(s: &str) -> (usize, usize)
     (min, max)
 }
 
-fn with_both(first: &str, second: &str, indentation: &str) -> String
+fn with_both(first: &str, second: &str) -> String
 {
+    let (_, max_len) = min_max_line_len(first);
+
     let first: Vec<&str> = first.split('\n').collect();
     let second: Vec<&str> = second.split('\n').collect();
     let mut s = String::new();
@@ -79,10 +62,17 @@ fn with_both(first: &str, second: &str, indentation: &str) -> String
         let fir = first.get(i).unwrap_or(&"");
         let sec = second.get(i).unwrap_or(&"");
 
-        s.push_str(&format!("{fir}{indentation}{sec}\n"));
+
+        s.push_str(&format!("{fir}"));
+
+        for _ in 0..=max_len - fir.len() + 2
+        {
+            s.push(' ');
+        }
+
+        s.push_str(&format!("{sec}\n"));
 
         i += 1;
     }
-
     s
 }
