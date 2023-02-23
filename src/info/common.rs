44a72ab119fa522e1da9aa12a79e3d2dd2246664
Aidan Neal <decator.c@proton.me>
2023-02-23 07:05:31 -0500

    start opengl

--- src/info/common.rs
+++ src/info/common.rs
@@ -1,3 +1,7 @@
+use ogl33::{glGetString, GLubyte, GL_VENDOR};
+
+use super::InfoError;
+
 pub fn ipv4_to_int(s: &str) -> u32
 {
     let mut s = s.split('.');
@@ -40,4 +44,14 @@ pub fn int_to_ipv4(i: u32) -> String
     let d = (i >> 24) & 0xFF;
 
     format!("{a}.{b}.{c}.{d}")
+}
+
+pub fn get_gpu_name_gl() -> Result<(), InfoError> {
+    let vendor_name: *const GLubyte = unsafe { glGetString(GL_VENDOR) };
+    if vendor_name.is_null()
+    {
+        return Err(InfoError::General("OPENGL: failed to return gpu vendor name".to_string()));
+    }
+
+    Ok(())
 }
