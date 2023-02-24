90bb6b86234358378d059ca3ea121cc5e9680922
Aidan Neal <decator.c@proton.me>
2023-02-23 20:19:43 -0500

    more of the same

--- lib/info/macos/caller.c
+++ lib/info/macos/caller.c
@@ -6,8 +6,12 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-int macos_get_caller(size_t pid, char* buffer, size_t buffer_len)
-{
+#include <string.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int macos_get_caller(size_t pid, char* buffer, size_t buffer_len) {
     int argmax;
     size_t argmax_size;
     char *procargs, *cp, *thiscmd;
@@ -16,12 +20,12 @@ int macos_get_caller(size_t pid, char* buffer, size_t buffer_len)
 
     argmax_size = sizeof(argmax);
     if (sysctl(mib, 2, &argmax, &argmax_size, NULL, 0) == -1) {
-        goto _end;
+        return -1;
     }
 
     procargs = malloc(argmax);
     if (procargs == NULL) {
-        goto _end;
+        return -1;
     }
 
     mib[0] = CTL_KERN;
@@ -31,45 +35,48 @@ int macos_get_caller(size_t pid, char* buffer, size_t buffer_len)
     argmax_size = (size_t)argmax;
     if (sysctl(mib, 3, procargs, &argmax_size, NULL, 0) == -1) {
         free(procargs);
-        goto _end;
+        return -1;
     }
 
-    /* Find the end of the first argument string */
-    for (cp = procargs; *cp != '\0'; cp++)
-        ;
+    for (cp = procargs; cp < &procargs[argmax_size]; cp++) {
+        if (*cp == '\0') {
+            break;
+        }
+    }
 
-    /* Find the start of the second argument string */
-    for (; cp < &procargs[argmax_size] && *cp == '\0'; cp++)
-        ;
+    if (cp == &procargs[argmax_size]) {
+        free(procargs);
+        return -1;
+    }
 
-    /* Find the end of the second argument string */
-    for (thiscmd = cp; cp < &procargs[argmax_size] && *cp != '\0'; cp++)
-        ;
+    for (; cp < &procargs[argmax_size]; cp++) {
+        if (*cp != '\0') {
+            break;
+        }
+    }
+
+    if (cp == &procargs[argmax_size]) {
+        free(procargs);
+        return -1;
+    }
 
     /* Strip off any path that was specified */
-    for (; thiscmd < cp && *thiscmd != '/'; thiscmd++)
-        ;
-
-    if (thiscmd == cp) {
-        free(procargs);
-        goto _end;
+    for (thiscmd = cp; (cp < &procargs[argmax_size]) && (*cp != '\0'); cp++) {
+        if (*cp == '/') {
+            thiscmd = cp + 1;
+        }
     }
 
-    size_t len = cp - thiscmd;
-    if (buffer_len <= len) {
+    if (buffer_len < strlen(thiscmd) + 1) {
         free(procargs);
         return -2;
     }
 
-    memcpy(buffer, thiscmd, len);
-    buffer[len] = '\0';
-
+    strcpy(buffer, thiscmd);
     free(procargs);
     return 0;
-
-_end:
-    return 0;
 }
+
 
 _end:
 
