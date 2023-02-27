996c38b169a3a26a7bcca60667142e8f7ffb98f7
Decator <decator.c@proton.me>
2023-02-27 14:20:03 -0500

    Comment and add common tests

--- src/info/common.rs
+++ src/info/common.rs
@@ -1,3 +1,4 @@
+/// Convert an IPV4 string to a big-endian u32
 pub fn ipv4_to_int(s: &str) -> u32
 {
     let mut octets = s.split('.').filter_map(|octet| {
@@ -12,6 +13,18 @@ pub fn ipv4_to_int(s: &str) -> u32
         }
     });
 
+
+    // We shift all elements to store each octet (u8) into a u32
+    // Example source:
+    // 192.168.1.19
+    //
+    // Example binary translation as separate `u8`'s:
+    // 11000000 10101000 00000001 00010011
+    //
+    // Example final u32:
+    // 11000000_10101000_00000001_00010011
+    // 
+    // To get this we shift everything into place then add everything.
     let a = octets.next().unwrap_or_default();
     let b = octets.next().unwrap_or_default() << 8;
     let c = octets.next().unwrap_or_default() << 16;
@@ -20,26 +33,36 @@ pub fn ipv4_to_int(s: &str) -> u32
     u32::to_be(a + b + c + d)
 }
 
+/// Convert a big-endian IPV4 address to a string.
 pub fn int_to_ipv4(i: u32) -> String
 {
-    use std::io::Write;
     let i = i.to_le();
-    let mut buf = [0u8; 15];
-    {
-        let mut cursor = std::io::Cursor::new(&mut buf[..]);
-        write!(
-            cursor,
-            "{}.{}.{}.{}",
-            i & 0xFF,
-            (i >> 8) & 0xFF,
-            (i >> 16) & 0xFF,
-            (i >> 24) & 0xFF
-        )
-        .unwrap();
-    }
-    String::from_utf8_lossy(&buf[..]).to_string()
-}
+    
+    // Example source:
+    // 11000000_10101000_00000001_00010011
+    //
+    // Example masks: 
+    // 11111111_00000000_00000000_00000000 : octet 1
+    // 00000000_11111111_00000000_00000000 : octet 2
+    // 00000000_00000000_11111111_00000000 : octet 3
+    // 00000000_00000000_00000000_11111111 : octet 4
+    //
+    // >> 24 to align the highest order element (the first element).
+    // We've shifted everything over, so we don't need to mask anything.
+    // >> 16 to align the second element, then mask off what we want.
+    // >> 8 to align the third octet, then mask what we want.
+    // Mask the final octet.
 
+    // 0xFF == 0b1111_1111
+    format!(
+        "{}.{}.{}.{}",
+        i >> 24,
+        (i >> 16) & 0xFF,
+        (i >> 08) & 0xFF,
+        i & 0xFF,
+    )
+    
+}
 /// An iterator that produces only the unique elements from the iterator it was
 /// called on
 struct Unique<'a, I>
@@ -86,3 +109,26 @@ trait UniqueIterator<'a>: Iterator<Item = &'a str>
 }
 
 impl<'a, I> UniqueIterator<'a> for I where I: Iterator<Item = &'a str> {}
+
+#[cfg(test)]
+mod cfg
+{
+    use super::*;
+    const IP1 = "8.8.8.8";
+    const IP2 = "192.168.1.19";
+
+    #[test]
+    fn test_ipv4_to_int()
+    {
+        assert_eq!(134744072, ipv4_to_int(IP1));
+        assert_eq!(3232235795, ipv4_to_int(IP2));
+    }
+
+    #[test]
+    fn test_int_to_ipv4()
+    {
+        assert_eq!(int_to_ipv4(134744072), IP1.to_string());
+        assert_eq!(int_to_ipv4(3232235795), IP2.to_string());
+    }
+
+}
